07d3abe409b19dd7116241e4409d33d669f60fdd
Lucas Silva Figueiredo <lsf@cin.ufpe.br>
2022-05-16 17:22:48 -0300

    iniciando integração com FXML no arquivo Test.main

--- src/ControladorNomePersonagem.java
+++ src/ControladorNomePersonagem.java
@@ -1,24 +1,39 @@
-import javafx.event.ActionEvent;
+import java.util.ArrayList;
+
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.TextArea;
 
 public class ControladorNomePersonagem {
 
     @FXML
-    private Button botaoIniciar;
+    private ButtonBar botoesEscolha;
 
     @FXML
-    private Label labelNomePersonagem;
+    private TextArea imagemAscii;
 
     @FXML
-    private TextField textFieldNomePersonagem;
+    private TextArea textoCapitulo;
 
-    @FXML
-    void salvarNomePersonagem(ActionEvent event) {
-        String nome = textFieldNomePersonagem.getText();
-        labelNomePersonagem.setText(nome);
-        textFieldNomePersonagem.setVisible(false);
+    public void mostrarTextoCapitulo(String texto) 
+    {
+        textoCapitulo.setText(texto);
+    }
+
+    public void mostrarImagemAscii(String imagem) 
+    {
+        imagemAscii.setText(imagem);
+    }
+
+    public void mostrarEscolhas(ArrayList<Escolha> escolhas)
+    {
+        botoesEscolha.setPadding(new Insets(10));
+
+        for (int i = 0; i < escolhas.size(); i++) {
+            botoesEscolha.getButtons().add(new Button(escolhas.get(i).getTextoMostrado()));
+            //System.out.println("- " + escolhas.get(i).getTextoMostrado());
+        }
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,15 @@
 import java.util.Map;
 import java.util.Scanner;
 
-public class Test 
-{    
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Test extends Application
+{
+
     public static void main(String[] args) 
     {
         Scanner escaneadorDoConsole = new Scanner(System.in, "CP850");
@@ -14,8 +21,21 @@ public class Test
         System.out.println("Carregamento finalizado\n\n...\n\n");
 
         Capitulo raiz = capitulos.get("raiz");
-        raiz.executar();
+        //raiz.executar();
 
         escaneadorDoConsole.close();
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("arquivoTeste.fxml"));
+        Parent root = loader.load();
+        Scene tela = new Scene(root);
+        
+        primaryStage.setTitle("Livro Interativo!");
+        primaryStage.setScene(tela);
+        primaryStage.show();
     }
 }
